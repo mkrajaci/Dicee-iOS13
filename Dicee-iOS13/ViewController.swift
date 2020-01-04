54b5d68264551d8cee78fcea89e233018c53084e
Mario Krajacic <krajacic.mario@gmail.com>
2020-01-04 13:39:57 +0100

    Added manipulation on variables.

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -13,16 +13,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
+    var leftDiceNumber = 0
+    var rightDiceNumber = 5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-    //  who            what     walue
-        diceImageView1.alpha = 0.5
-        diceImageView1.image = #imageLiteral(resourceName: "DiceFour") // behind = is imageliteral property, it is used for selecting images in assets
-        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
+    // changing properties on objects
+    /* who            what     walue
+       diceImageView1.alpha = 0.5
+       diceImageView1.image = #imageLiteral(resourceName: "DiceFour") // behind = is imageliteral property, it is used for selecting images in assets
+    */
+        diceImageView1.image = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][leftDiceNumber]
+        diceImageView2.image = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][rightDiceNumber]
+        leftDiceNumber += 1
+        rightDiceNumber -= 1
     }
 }
 
