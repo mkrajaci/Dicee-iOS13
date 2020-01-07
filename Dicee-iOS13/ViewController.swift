5bf6bb19682aea44ed895479691623d390a6abd6
Mario Krajacic <krajacic.mario@gmail.com>
2020-01-07 06:24:22 +0100

    Randomizeing dice array.

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -12,14 +12,6 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
-    
-    var leftDiceNumber = 0
-    var rightDiceNumber = 5
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
     // changing properties on objects
@@ -27,10 +19,9 @@ class ViewController: UIViewController {
        diceImageView1.alpha = 0.5
        diceImageView1.image = #imageLiteral(resourceName: "DiceFour") // behind = is imageliteral property, it is used for selecting images in assets
     */
-        diceImageView1.image = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][leftDiceNumber]
-        diceImageView2.image = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][rightDiceNumber]
-        leftDiceNumber += 1
-        rightDiceNumber -= 1
+        let diceAray = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        diceImageView1.image = diceAray.randomElement()
+        diceImageView2.image = diceAray.randomElement()
     }
 }
 
